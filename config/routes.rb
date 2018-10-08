b72cb7566f7fd7fe19f8b8b022952b3e79378408
helium <wetcondition@gmail.com>
2018-10-08 17:23:01 +0900

    フィード投稿

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   resources :users, only: %i[show new create]
+  resources :feeds, only: %i[index new create]
 end
