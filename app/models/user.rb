bc4781c73b260bda940802184d5940e5d300becc
helium <wetcondition@gmail.com>
2018-10-08 16:33:23 +0900

    ユーザ登録バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+
+  before_validation { email.downcase! }
+  validates :email, uniqueness: true,
+                    presence: true,
+                    length: { maximum: 50 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
   has_secure_password
+  validates :password_digest, presence: true, length: { minimum: 6 }
 end
