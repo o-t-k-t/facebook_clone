28454f0d33a56b31cae2a00b7b0642abda0535e5
helium <wetcondition@gmail.com>
2018-10-10 12:50:06 +0900

    ユーザ登録時の不正リダイレクトを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_logged_in
+  before_action :require_logged_in, except: %i[new create]
 
   def show
     @user = User.find(params[:id])
